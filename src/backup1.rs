8aa8bae862cfc0f524cea7c6c38b0a026ffa56fe
Jakob Peters <jakobj.peters@gmail.com>
2023-10-26 07:18:48 -0700

    Combine boids into single mesh

--- src/backup1.rs
+++ src/backup1.rs
@@ -1,13 +1,22 @@
 
-use std::{iter::zip, f32::consts::PI, cmp::Ordering};
+use std::f32::consts::PI;
 use rand::random;
 use itertools::Itertools;
 use bevy::{
     prelude::*,
-    sprite::{Mesh2dHandle, MaterialMesh2dBundle},
+    reflect::{TypeUuid, TypePath},
+    sprite::Mesh2dHandle,
+    asset::{Asset, Handle},
     render::mesh::PrimitiveTopology::TriangleList,
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}
 };
+
+#[derive(Component)]
+// struct Boids<M> where M: TypeUuid + TypePath {
+struct Boids<M> where M: Asset {
+    mesh: Handle<Mesh>,
+    material: Handle<M>
+}
 
 fn main() {
     App::new()
@@ -34,35 +43,49 @@ fn setup(
 
     let points = [17., 19.]
         .map(|x| {
-                let theta = PI / 12. * x;
-                16. * (theta.cos() * Vec2::X + theta.sin() * Vec2::Y)
-            });
-    let vertices = (1..100)
+            let theta = PI / 12. * x;
+            16. * (theta.cos() * Vec2::X + theta.sin() * Vec2::Y)
+        });
+    let vertices: Vec<[f32; 3]> = (1..100)
         .flat_map(|_| {
-                let position = 512. * (Vec2::new(random(), random()) - 0.5);
-                let rotation = Vec2::from_angle(2. * PI * random::<f32>());
-                [rotation.rotate(points[0]), rotation.rotate(points[1]), Vec2::ZERO]
-                    .map(|point| (point + position).extend(0.).to_array())
-            })
-        .collect::<Vec<[f32; 3]>>();
+            let position = 512. * (Vec2::new(random(), random()) - 0.5);
+            let rotation = Vec2::from_angle(2. * PI * random::<f32>());
+            [rotation.rotate(points[0]), rotation.rotate(points[1]), Vec2::ZERO]
+                .map(|point| (point + position).extend(0.).to_array())
+        })
+        .collect();
 
     let mut mesh = Mesh::new(TriangleList);
     mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
 
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(mesh).into(),
-        material: materials.add(ColorMaterial::from(Color::BLUE)),
-        ..default()
+    commands.spawn(Boids {
+        mesh: meshes.add(mesh),
+        material: materials.add(ColorMaterial::from(Color::BLUE))
     });
+
+    // commands.spawn((
+    //     materials.add(ColorMaterial::from(Color::BLUE)),
+    //     meshes.add(mesh)
+    // ));
+
+    // commands.spawn(MaterialMesh2dBundle {
+    //     material: materials.add(ColorMaterial::from(Color::BLUE)),
+    //     mesh: meshes.add(mesh).into(),
+    //     ..default()
+    // });
 }
 
-fn update(query: Query<&Mesh2dHandle>, assets: ResMut<Assets<Mesh>>, _time: Res<Time>) {
-    let mesh = assets
-        .into_inner()
-        .get_mut(&query.single().0)
-        .unwrap();
+// fn update(boids: ResMu/t<Assets<Mesh>>, _time: Res<Time>) {
+fn update<M: Asset>(boids: Boids<M>, _time: Res<Time>) {
+    // let mesh = boids
+    //     .into_inner()
+    //     .iter_mut()
+    //     .at_most_one()
+    //     .unwrap_or_else(|_| None)
+    //     .unwrap()
+    //     .1;
 
-    let vertices = mesh
+    let vertices = boids.mesh
         .attribute(Mesh::ATTRIBUTE_POSITION)
         .unwrap() 
         .as_float3()
@@ -70,20 +93,9 @@ fn update(query: Query<&Mesh2dHandle>, assets: ResMut<Assets<Mesh>>, _time: Res<
         .into_iter()
         .chunks(3)
         .into_iter()
-        .flat_map(|chunk| {
-                let vertices = chunk
-                    .map(|vertex| *vertex)
-                    .collect::<Vec<[f32; 3]>>();
-
-                vertices
-                    .iter()
-                    .map(|vertex| [
-                            vertex[0] + (0.05 * (vertices[0][1] - vertices[1][1])),
-                            vertex[1] + (-0.05 * (vertices[0][0] - vertices[1][0])),
-                            0.
-                        ])
-                    .collect::<Vec<[f32; 3]>>()
-            })
+        .flat_map(|triangle| {
+            triangle.map(|vertex| *vertex)
+        })
         .collect::<Vec<[f32; 3]>>();
 
     mesh.remove_attribute(Mesh::ATTRIBUTE_POSITION);
