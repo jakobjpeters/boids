f6c6d7736546bb9f1deb7c2e27bb3496dc5bb295
Jakob Peters <jakobj.peters@gmail.com>
2023-10-27 07:36:44 -0700

    Implement alignment

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 
-use std::f32::consts::PI;
+use std::{f32::{consts::PI, EPSILON}, iter::zip};
 use rand::random;
-use bevy::{prelude::*, render::mesh, sprite::MaterialMesh2dBundle};
+use bevy::{
+    prelude::*,
+    render::mesh,
+    sprite::MaterialMesh2dBundle,
+    diagnostic::{LogDiagnosticsPlugin, FrameTimeDiagnosticsPlugin}
+};
 
-const BORDER: f32 = 256.;
-const BOIDS: usize = 10;
+const BORDER: f32 = 512.;
+const BOIDS: usize = 32;
+const SCALE: f32 = 8.;
+const SPEED: f32 = 100.;
+const ALIGNMENT: f32 = 1.;
 
 #[derive(Component)]
 struct Boid;
@@ -21,7 +29,8 @@ fn main() {
         .add_plugins(LogDiagnosticsPlugin::default())
         .add_plugins(FrameTimeDiagnosticsPlugin::default())
         .add_systems(Startup, setup)
-        .add_systems(Update, update)
+        .add_systems(Update, popup)
+        .add_systems(Update, boids)
         .run();
 }
 
@@ -30,7 +39,7 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    for _ in 1..BOIDS {
+    for _ in 1..BOIDS + 1 {
         let mut mesh = Mesh::new(mesh::PrimitiveTopology::TriangleList);
         mesh.insert_attribute(
             Mesh::ATTRIBUTE_POSITION,
@@ -47,21 +56,86 @@ fn setup(
                         2. * BORDER * random::<f32>() - BORDER,
                     0.),
                     rotation: Quat::from_rotation_z(2.0 * PI * random::<f32>()),
-                    scale: Vec3::splat(8.)
+                    scale: Vec3::splat(SCALE)
                 },
                 material: materials.add(ColorMaterial::from(Color::BLUE)),
+                visibility: Visibility::Hidden,
                 ..default()
             }
         ));
     }
 
+    commands.spawn(Text2dBundle {
+        text: Text::from_sections([
+            TextSection::new(
+                "Boids\n",
+                TextStyle {
+                    font_size: 64.,
+                    ..default()
+                }
+            ),
+            TextSection::new(
+                "Press `escape` to start the simulation\n\n",
+                TextStyle {
+                    font_size: 48.,
+                    ..default()
+                }
+            ),
+            TextSection::new(
+                "Separation: boids should avoid crowding nearby boids.\nAlignment: boids should fly in the same direction as nearby boids.\nCohesion: boids should be near other boids.",
+                TextStyle {
+                    font_size: 32.,
+                    ..default()
+                }
+            )
+        ]),
+        visibility: Visibility::Visible,
+        ..default()
+    });
+
     commands.spawn(Camera2dBundle::default());
 }
 
-fn update(mut transforms: Query<&mut Transform, With<Boid>>, time: Res<Time>) {
-    for mut transform in &mut transforms {
+fn popup(mut visibilities: Query<&mut Visibility>, keys: Res<Input<KeyCode>>) {
+    if keys.just_pressed(KeyCode::Escape) {
+        visibilities
+            .for_each_mut(|mut visibility| {
+                *visibility = match *visibility {
+                Visibility::Visible => Visibility::Hidden,
+                Visibility::Hidden => Visibility::Visible,
+                Visibility::Inherited => Visibility::Inherited
+            };
+        })
+    }
+}
+
+fn boids(mut transforms: Query<&mut Transform, With<Boid>>, time: Res<Time>) {
+    let velocities = transforms
+        .iter()
+        .map(|&a| transforms
+                .iter()
+                .filter(|b| a != **b)
+                .map(|b| b.local_y() / a.translation.distance(b.translation))
+                .sum::<Vec3>()
+                .normalize_or_zero()
+            )
+        .collect::<Vec<Vec3>>();
+
+    for (mut transform, velocity) in zip(&mut transforms, velocities) {
+        let a = if velocity == Vec3::ZERO { transform.local_y() } else {
+            velocity
+        };
+
+        let forward_dot = transform.local_y().dot(a);
+        let right = transform.rotation * Vec3::X;
+        let right_dot = right.dot(a);
+        let sign = -f32::copysign(1.0, right_dot);
+        let max_angle = forward_dot.clamp(-1.0, 1.0).acos();
+        let rotation_angle = sign * (ALIGNMENT * time.delta_seconds()).min(max_angle);
+        transform.rotate_z(rotation_angle);
+
         transform.translation = (
-            transform.translation + transform.local_y() * 100. * time.delta_seconds() + BORDER
+            transform.translation + SPEED * transform.local_y() * time.delta_seconds() + BORDER
         ).rem_euclid(2. * BORDER * Vec3::ONE) - BORDER;
     }
 }
