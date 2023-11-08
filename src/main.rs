63fd1e11f7673846ec97305bb878055e26e4f2c0
Jakob Peters <jakobj.peters@gmail.com>
2023-11-07 20:49:32 -0800

    Implement pausing

--- src/main.rs
+++ src/main.rs
@@ -15,25 +15,48 @@ const SPEED: f32 = 128.;
 const ALIGNMENT: f32 = 8.;
 const COHESION: f32 = 1.;
 const SEPARATION: f32 = 64.;
+const EXPLANATION: &str = r#"
+This is the start menu. After you press `escape`, the simulation will start.
+Each triangle represents a `boid`, which is a simulated bird.
+They will move forward and adjust their direction depending on three parameters:
+
+1) Separation: boids avoid crashing into each other
+2) Alignment: boids rotate to face the same direction as nearby boids
+3) Cohesion: boids try to flock together in groups
+
+You can adjust how strong each of these parameters are by moving the sliders on the ride-hand side of the simulation.
+Once the simulation is running, you can return to this menu by pressing `escape` again.
+"#;
 
 #[derive(Component)]
 struct Boid;
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+enum AppState {
+    #[default]
+    Menu,
+    Play,
+    Pause
+}
+
+fn main() { App::new()
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "boids".into(),
                 ..default()
             }),
             ..default()
         }))
-        .add_plugins(LogDiagnosticsPlugin::default())
-        .add_plugins(FrameTimeDiagnosticsPlugin::default())
-        .add_systems(Startup, setup)
-        .add_systems(Update, popup)
-        .add_systems(Update, boids)
-        .run();
+    .add_plugins(LogDiagnosticsPlugin::default())
+    .add_plugins(FrameTimeDiagnosticsPlugin::default())
+    .add_state::<AppState>()
+    .add_systems(Startup, setup)
+    .add_systems(Update, menu.run_if(in_state(AppState::Menu)))
+    .add_systems(Update, play.run_if(in_state(AppState::Play)))
+    .add_systems(Update, pause.run_if(in_state(AppState::Pause)))
+    .add_systems(OnEnter(AppState::Menu), transition)
+    .add_systems(OnExit(AppState::Menu), transition)
+    .run();
 }
 
 fn setup(
@@ -61,7 +84,7 @@ fn setup(
                     scale: Vec3::splat(SCALE)
                 },
                 material: materials.add(ColorMaterial::from(Color::BLUE)),
-                visibility: Visibility::Hidden,
+                visibility: Visibility::Visible,
                 ..default()
             }
         ));
@@ -84,31 +107,34 @@ fn setup(
                 }
             ),
             TextSection::new(
-                "Separation: boids should avoid crowding nearby boids.\nAlignment: boids should fly in the same direction as nearby boids.\nCohesion: boids should be near other boids.",
+                EXPLANATION,
                 TextStyle {
                     font_size: 32.,
                     ..default()
                 }
             )
         ]),
-        visibility: Visibility::Visible,
+        visibility: Visibility::Hidden,
         ..default()
     });
 
     commands.spawn(Camera2dBundle::default());
 }
 
-fn popup(mut visibilities: Query<&mut Visibility>, keys: Res<Input<KeyCode>>) {
-    if keys.just_pressed(KeyCode::Escape) {
-        visibilities
-            .for_each_mut(|mut visibility| {
-                *visibility = match *visibility {
-                Visibility::Visible => Visibility::Hidden,
-                Visibility::Hidden => Visibility::Visible,
-                Visibility::Inherited => Visibility::Inherited
-            };
-        })
-    }
+fn transition(mut visibilities: Query<&mut Visibility>) {
+    visibilities.for_each_mut(|mut visibility| *visibility = match *visibility {
+        Visibility::Visible => Visibility::Hidden,
+        Visibility::Hidden => Visibility::Visible,
+        Visibility::Inherited => Visibility::Inherited
+    })
+}
+
+fn menu(mut next_state: ResMut<NextState<AppState>>, keys: Res<Input<KeyCode>>) {
+    next_state.set(match keys.get_just_pressed().next() {
+        Some(KeyCode::Escape) => AppState::Play,
+        Some(KeyCode::Space) => AppState::Pause,
+        _ => AppState::Menu
+    });
 }
 
 fn rotate(transform: &mut Transform, x: Vec3, scale: f32) { if x != Vec3::ZERO {
@@ -118,7 +144,18 @@ fn rotate(transform: &mut Transform, x: Vec3, scale: f32) { if x != Vec3::ZERO {
     transform.rotate_z(-f32::copysign(1.0, right_dot) * scale.min(forward_dot.clamp(-1.0, 1.0).acos()));
 } }
 
-fn boids(mut transforms: Query<&mut Transform, With<Boid>>, time: Res<Time>) {
+fn play(
+    mut next_state: ResMut<NextState<AppState>>,
+    keys: Res<Input<KeyCode>>,
+    mut transforms: Query<&mut Transform, With<Boid>>,
+    time: Res<Time>
+) {
+    next_state.set(match keys.get_just_pressed().next() {
+        Some(KeyCode::Escape) => AppState::Menu,
+        Some(KeyCode::Space) => AppState::Pause,
+        _ => AppState::Play
+    });
+
     let xs = transforms
         .iter()
         .map(|&a| transforms
@@ -146,4 +183,12 @@ fn boids(mut transforms: Query<&mut Transform, With<Boid>>, time: Res<Time>) {
             transform.translation + SPEED * transform.local_y() * time.delta_seconds() + BORDER
         ).rem_euclid(2. * BORDER * Vec3::ONE) - BORDER;
     }
+}
+
+fn pause(mut next_state: ResMut<NextState<AppState>>, keys: Res<Input<KeyCode>>) {
+    next_state.set(match keys.get_just_pressed().next() {
+        Some(KeyCode::Escape) => AppState::Menu,
+        Some(KeyCode::Space) => AppState::Play,
+        _ => AppState::Pause
+    });
 }
